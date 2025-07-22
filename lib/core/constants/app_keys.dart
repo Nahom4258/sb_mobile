b644713a25951940d055ac88302bc6cd932e497f
Nahom4258 <nhabtamu42@gmail.com>
2025-07-22 21:04:30 +0300

    envs

---
+++ lib/core/constants/app_keys.dart
@@ -0,0 +1,7 @@
+const baseUrl =
+    'https://skillbridge-ij7jnmwh2q-zf.a.run.app/api/v1';
+
+const authenticationKey = 'authentication_key';
+const appInitializationKey = 'app_initilization_key';
+const usersDeviceTokenCollection = 'users_device_token';
+const languageKey = 'localization_key';
